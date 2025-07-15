e4d8587b9f7821936738e77bf55890a2fb71d769
olavea <ola@olavea.com>
2025-07-15 10:33:02 +0200

    I ran php artisan migrate, now I have stuff in my database

---
+++ resources/views/omvei.blade.php
@@ -0,0 +1,9 @@
+
+
+
+<h2>
+    <strong>Add one Something to your omvei</strong> 
+</h2>
+
+<p>// A dropdown with providers</p>
+<p>// A save button</p>
---
+++ resources/views/omveier.blade.php
@@ -0,0 +1,3 @@
+<p>// A new omvei button</p>
+
+<p>// A list of your omveis button</p>
