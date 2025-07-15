2c5b503c29b4ebae5f4d1263b1488c720fbbcbe1
olavea <ola@olavea.com>
2025-07-15 10:21:46 +0200

    db is empty, so this doesn't work

---
+++ resources/views/omvei.blade.php
@@ -0,0 +1,7 @@
+
+<h2>
+    <strong>Add one something to your name omvei</strong> 
+</h2>
+
+<p>// A dropdown with somethings</p>
+<p>// A save button</p>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
+use App\Models\User;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
+
+Route::get('omvei', function () {
+    return view('omvei');
+});
 
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
 
 Route::get('mineomveier', function () {
-    return view('mineomveier');
+    $users = Post::all();
+
+    dd($users);
 });
